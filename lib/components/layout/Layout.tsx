796f07b75777c0881073465ae4954bcf23c358ee
alxgrn <alex@molner.ru>
2024-12-16 14:11:23 +0300

    onLogoClick in Header

--- lib/components/layout/Layout.tsx
+++ lib/components/layout/Layout.tsx
@@ -2,21 +2,23 @@ import React, { FC, PropsWithChildren, useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Content from './Content';
-import './Layout.css';
 import { Sidebar } from '../../main';
+import './Layout.css';
 
 type Props = {
 	mainMenu: React.ReactNode;
 	userMenu: React.ReactNode;
+	onLogoClick?: () => void;
 }
 
-const Layout: FC<PropsWithChildren<Props>> = ({ children, mainMenu, userMenu }) => {
+const Layout: FC<PropsWithChildren<Props>> = ({ children, mainMenu, userMenu, onLogoClick }) => {
 	const [ isMenuVisible, setIsMenuVisible ] = useState(false);
 	return (
 		<div className='Layout'>
             <Header
 				userMenu={userMenu}
 				onMenuSwitch={() => setIsMenuVisible(b => !b)}
+				onLogoClick={onLogoClick}
 			/>
             <Content menu={mainMenu}>
 				{children}
